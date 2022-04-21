34c93c2081defa4ddfd5062379e103e157ba98d8
David Fischer <dfischer@fischerltd.com>
2022-04-21 18:45:08 -0400

    2nd commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
+let answer = 0
 
 // Your Code Here
 
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
-console.log(answer)
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
 
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
 
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
