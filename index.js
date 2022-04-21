3451464c1a418f99a635bf4adbe4b90c009e5664
David Fischer <dfischer@fischerltd.com>
2022-04-21 17:45:39 -0400

    1st commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let annualExpenses = 0
 
 // Your Code Here
 
-let answer = window.prompt(weeklyExpenseQuestions[0])
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
 console.log(answer)
+
 
